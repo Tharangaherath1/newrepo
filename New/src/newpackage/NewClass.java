b7da300c79c8cfde64fa1f387bc9d0ac12ba3a18
Tharangaherath1 <prabodhaniherath@gmail.com>
2021-07-12 20:51:00 -0700

    print add

--- New/src/newpackage/NewClass.java
+++ New/src/newpackage/NewClass.java
@@ -12,5 +12,6 @@ package newpackage;
 public class NewClass {
     public static void main(String [] args){
         System.out.println("Hello World");
+        System.out.println("hgkfdgks");
     }
 }
